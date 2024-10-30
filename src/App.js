50086ba28258afe0c9a30dc03364474dac7c35b8
aryan27270 <aryan1234choudhari1983@gmail.com>
2024-10-30 10:40:25 +0530

    "Added keys to News components in App.js and added trailing spaces to self-closing tags."

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ export default class App extends Component {
     return (
       <div>
         <Router>
-          <Navbar/>
+          <Navbar />
           <Routes>
-            <Route path='/' element={<News category='general'/>} />
-            <Route path='/business' element={<News key='business' category='business'/>} />
-            <Route path='/entertainment' element={<News key='entertainment' category='entertainment'/>} />
-            <Route path='/health' element={<News key='health' category='health'/>} />
-            <Route path='/science' element={<News key='science' category='science'/>} />
-            <Route path='/sports' element={<News key='sports' category='sports'/>} />
-            <Route path='/technology' element={<News key='technology' category='technology'/>} />
+            <Route path='/' element={<News key='general' category='general' />} />
+            <Route path='/business' element={<News key='business' category='business' />} />
+            <Route path='/entertainment' element={<News key='entertainment' category='entertainment' />} />
+            <Route path='/health' element={<News key='health' category='health' />} />
+            <Route path='/science' element={<News key='science' category='science' />} />
+            <Route path='/sports' element={<News key='sports' category='sports' />} />
+            <Route path='/technology' element={<News key='technology' category='technology' />} />
           </Routes>
         </Router>
       </div>
